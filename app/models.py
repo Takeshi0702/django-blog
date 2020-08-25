012eec26e662c1c15560a52eb17d3800b6569a02
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-08-25 22:37:03 +0900

    Django学習ステップ1

--- app/models.py
+++ app/models.py
@@ -4,5 +4,11 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title = models.CharField("タイトル", max_length=200)
+	content = models.TextField("本文")
+	created = models.DateTimeField("作成日", default=timezone.now)
+
+	def __str__(self):
+		return self.title
     
